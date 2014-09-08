cfbb88b90754f072a106f04cc7d893d100d0d5f6
moof mayeda & mark rubenstein <student@epicodus.com>
2014-09-08 14:01:23 -0700

    display errors for invalid numbers

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
-describe Message do
+describe Message, vcr: true do
   it "sends a message" do
     message = Message.new(body: "hey", to: '2073709461', from: '15005550006')
     expect(message.save).to eq true
+  end
+
+  it 'adds an error if the to number is invalid' do
+    message = Message.new(body: 'hi', to: '1111111', from: '15005550006')
+    message.save
+    expect(message.errors.messages[:base]).to eq ["The 'To' number 1111111 is not a valid phone number."]
+  end
+
+  it 'adds an error if the from number is invalid' do
+    message = Message.new(body: 'hi', to: '2073709461', from: '555')
+    message.save
+    expect(message.errors.messages[:base]).to eq ["The From phone number 555 is not a valid, SMS-capable inbound phone number or short code for your account."]
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -42,4 +43,12 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.filter_sensitive_data('<twilio account sid>') { ENV['TWILIO_ACCOUNT_SID'] }
+  c.filter_sensitive_data('<twilio auth token>') { ENV['TWILIO_AUTH_TOKEN'] }
 end
