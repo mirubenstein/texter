4414afddb60f258f48e01b2d88beb9501728836d
moof mayeda & mark rubenstein <student@epicodus.com>
2014-09-09 13:50:24 -0700

    separate message and contact routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,31 +4,29 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @contact = Contact.find(params[:contact_id])
     @message = Message.new
   end
 
   def create
-    @contact = Contact.find(params[:contact_id])
     phones = message_params[:to]
     phones.pop
     phones.each do |phone|
+      contact = Contact.find_by(phone: phone)
       @message = Message.new(message_params)
       @message.to = phone
       if @message.save
-        @contact.messages << @message
+        contact.messages << @message
       end
     end
     if @message.errors.any?
       render 'new'
     else
       flash[:notice] = "Message sent!"
-      redirect_to contact_message_path(@contact, @message)
+      redirect_to message_path(@message)
     end
   end
 
   def show
-    @contact = Contact.find(params[:contact_id])
     @message = Message.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :contacts, only: [:new, :create, :show, :index] do
-    resources :messages, only: [:new, :create, :show, :index]
-  end
+  resources :contacts, only: [:new, :create, :show, :index]
+  resources :messages, only: [:new, :create, :show, :index]
 
   match('/inbound_messages', {via: :post, to: 'replies#create', as: :inbound_messages})
 end
